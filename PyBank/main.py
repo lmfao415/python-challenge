04771e8c8d3999e2cdf7ff3ae87628f543d05146
rmurnane <70532886+lmfao415@users.noreply.github.com>
2020-11-01 23:36:09 -0800

    Added month calculator to python file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,18 @@
 import os
 import csv
 
- month = 0
-
+month = 0
+total = 0
 csvpath = os.path.join('Resources', 'budget_data.csv')
 with open(csvpath, "r") as csvfile:   
     csvreader = csv.reader(csvfile, delimiter=",")
 
     header = next(csvreader)
    
-    
+    for row in csvreader:
+        month = month + 1
+        
+
+print(f"Total Months: {month}")
 
     
