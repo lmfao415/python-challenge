68d60ce17c56fe6d1766a0aba84a8e5d0473efeb
rmurnane <70532886+lmfao415@users.noreply.github.com>
2020-11-02 10:53:11 -0800

    Added greatest increase and decrease in profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,9 +3,13 @@ import csv
 
 
 total_months = 0
-total = 0
+total_profit = 0
 
-
+max_value = -999999999
+min_value = 999999999
+max_month = ""
+min_month = ""
+last_profit = 0
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 with open(csvpath, "r") as csvfile:   
@@ -14,10 +18,24 @@ with open(csvpath, "r") as csvfile:
     header = next(csvreader)
    
     for row in csvreader:
+        profit = int(row[1])
+        month = row[0]
+        if total_months == 0:
+            last_profit = profit
+        else:
+            monthly_change = profit - last_profit
+            if monthly_change < min_value:
+                min_value = monthly_change
+                min_month = month
+            if monthly_change > max_value:
+                max_value = monthly_change
+                max_month = month
+            last_profit = profit
         total_months += 1
-        total = total + int(row[1])
-
+        total_profit = total_profit + profit
+print("Financial Analysis")
+print("-------------------------------")
 print(f"Total Months: {total_months}")
-print(f"Total Profit: ${total:0,.2f}")
-
-    
+print(f"Total Profit: ${total_profit:0,.2f}")
+print(f"max: {max_month} ${max_value:0,.2f}")
+print(f"min: {min_month} {min_value:0,.2f}")
