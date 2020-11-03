2b160de764417cf720a28425f73e2f204e861038
rmurnane <70532886+lmfao415@users.noreply.github.com>
2020-11-03 10:27:01 -0800

    Added comments to python files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,40 +1,41 @@
 import os
-import csv
+import csv  #import dependencies
 
 total_months = 0
 total_profit = 0
-
 max_value = -999999999
 min_value = 999999999
 max_month = ""
 min_month = ""
 last_profit = 0
-first_profit = 0
+first_profit = 0  #set initial variables
+
 csvpath = os.path.join('Resources', 'budget_data.csv')
 with open(csvpath, "r") as csvfile:   
-    csvreader = csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter=",") #open csv file
 
-    header = next(csvreader)
+    header = next(csvreader)  #skip/save the header
    
-    for row in csvreader:
+    for row in csvreader:  #read through each row in csv file
         profit = int(row[1])
         month = row[0]
-        if total_months == 0:
+        if total_months == 0:  
             last_profit = profit
             first_profit = profit
         else:
-            monthly_change = profit - last_profit
-            if monthly_change < min_value:
+            monthly_change = profit - last_profit #calculate's the monthly change
+            if monthly_change < min_value:  #updates greatest decrease value
                 min_value = monthly_change
                 min_month = month
             if monthly_change > max_value:
-                max_value = monthly_change
+                max_value = monthly_change #updates greatest increase value
                 max_month = month
             last_profit = profit
-        total_months += 1
-        total_profit = total_profit + profit
-    average_profit_change = (last_profit - first_profit) / (total_months - 1)
-print("Financial Analysis")
+        total_months += 1 #calculates the total months
+        total_profit = total_profit + profit #calculates the total profit
+    average_profit_change = (last_profit - first_profit) / (total_months - 1) #calculates average monthly change
+
+print("Financial Analysis") #prints results to terminal
 print("-------------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total Profit: ${total_profit:0,.2f}")
@@ -42,7 +43,7 @@ print(f"Average Monthly Change: ${average_profit_change:0,.2f}")
 print(f"Greatest Increase in Monthly Profits: ({max_month} : ${max_value:0,.2f})")
 print(f"Greatest Decrease in Monthly Profits: ({min_month} : ${min_value:0,.2f})")
 
-output_path = os.path.join("analysis", "budget_analysis")
+output_path = os.path.join("analysis", "budget_analysis") #creates new txt file with results
 with open(output_path, 'w', newline='') as txtfile:
     txtfile.write(f"Financial Analysis\n------------------------------- \n")
     txtfile.write(f"Total Months: {total_months} \n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,38 +1,37 @@
 import os
-import csv
+import csv #import dependencies
 
 csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath, "r") as csvfile:   
-    csvreader = csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter=",") #read csv file
 
-    header = next(csvreader)
-    candidates ={}
+    header = next(csvreader) #save/skip csv header
+    candidates ={} #set a dictionary to hold candidates and votes
     total_votes = 0
     
-    for row in csvreader:
-        if row[2] not in candidates:
+    for row in csvreader:  #read through each row in csv file
+        if row[2] not in candidates:  #adds candidate to dictionary if not included already
             candidates[row[2]] = 0
-        candidates[row[2]] +=1
-        total_votes += 1
+        candidates[row[2]] +=1 #updates the amount of votes a candidate has
+        total_votes += 1 #calculates the number of total votes
 
-print(f"Election Results")
+print(f"Election Results") # prints the results to terminal
 print(f"-------------------------")
 print(f"Total Votes: {total_votes}")
 print(f"-------------------------")
-for x in candidates:
+for x in candidates:  #goes through candidates dictionary and reports values
     print(f"{x}: {(candidates[x] / total_votes*100):0,.3f}% ({candidates[x]})")
-
 print(f"-------------------------")
-print(f"Winner: {max(candidates, key=candidates.get)}")
+print(f"Winner: {max(candidates, key=candidates.get)}") #reports the candidate with the highest number of votes
 print(f"-------------------------")
 
-output_path = os.path.join("analysis", "election_results")
+output_path = os.path.join("analysis", "election_results") #prints reults to txt file
 with open(output_path, 'w', newline='') as txtfile:
     txtfile.write(f"Election Results\n------------------------------- \n")
     txtfile.write(f"Total Votes: {total_votes}\n-------------------------------\n")
-    for x in candidates:
-        txtfile.write(f"{x}: {(candidates[x] / total_votes*100):0,.3f}% ({candidates[x]})\n")
+    for x in candidates: #goes through candidates dictionary and reports values
+        txtfile.write(f"{x}: {(candidates[x] / total_votes*100):0,.3f}% ({candidates[x]})\n") 
+    txtfile.write(f"Winner: {max(candidates, key=candidates.get)}\n") #reports the candidate with the highest number of votes
     txtfile.write(f"-------------------------------\n")
-    txtfile.write(f"Winner: {max(candidates, key=candidates.get)}\n")
-    txtfile.write(f"-------------------------------")
+   
 
