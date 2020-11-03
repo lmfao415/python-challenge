566bb15868c3deb0706d2f4258dc4f03ade43ccb
rmurnane <70532886+lmfao415@users.noreply.github.com>
2020-11-03 09:19:29 -0800

    Calculated election results and made txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,38 @@
 import os
 import csv
-total_votes = 0
 
 csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath, "r") as csvfile:   
     csvreader = csv.reader(csvfile, delimiter=",")
 
     header = next(csvreader)
+    candidates ={}
+    total_votes = 0
     
     for row in csvreader:
+        if row[2] not in candidates:
+            candidates[row[2]] = 0
+        candidates[row[2]] +=1
         total_votes += 1
 
+print(f"Election Results")
+print(f"-------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"-------------------------")
+for x in candidates:
+    print(f"{x}: {(candidates[x] / total_votes*100):0,.3f}% ({candidates[x]})")
 
-print(f"Total Votes : {total_votes}")
+print(f"-------------------------")
+print(f"Winner: {max(candidates, key=candidates.get)}")
+print(f"-------------------------")
+
+output_path = os.path.join("analysis", "election_results")
+with open(output_path, 'w', newline='') as txtfile:
+    txtfile.write(f"Election Results\n------------------------------- \n")
+    txtfile.write(f"Total Votes: {total_votes}\n-------------------------------\n")
+    for x in candidates:
+        txtfile.write(f"{x}: {(candidates[x] / total_votes*100):0,.3f}% ({candidates[x]})\n")
+    txtfile.write(f"-------------------------------\n")
+    txtfile.write(f"Winner: {max(candidates, key=candidates.get)}\n")
+    txtfile.write(f"-------------------------------")
+
